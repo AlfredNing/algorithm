59b1249899edf0aabc9242cff72d5a756a605fe4
nq <15667269283@163.com>
2022-06-03 10:07:07 +0800

    博弈论yyds

--- src/main/java/stage01/lesson10/Knapsack.java
+++ src/main/java/stage01/lesson10/Knapsack.java
@@ -17,6 +17,7 @@ public class Knapsack {
 
     /**
      * 返回货物的最大价值
+     *
      * @param w        重量数组
      * @param v        价值
      * @param index    当前选择的货物
@@ -25,19 +26,37 @@ public class Knapsack {
      * @return -1 认为没有方案
      */
     private static int process(int[] w, int[] v, int index, int alreadyW, int bag) {
-        if (alreadyW > bag){
+        if (alreadyW > bag) {
             return -1;
         }
-        if (index == w.length){
+        if (index == w.length) {
             return 0; // 方案有效，但已经没货了
         }
         // 没有选在index所在货物
         int p1 = process(w, v, index + 1, alreadyW, bag);
-        int p2Next = process(w, v, index + 1, alreadyW +w[index], bag);
+        int p2Next = process(w, v, index + 1, alreadyW + w[index], bag);
 
         int p2 = -1;
-        if (p2Next != -1){
+        if (p2Next != -1) {
             p2 = v[index] + p2Next;
+        }
+        return Math.max(p1, p2);
+    }
+
+    // rest代表剩余空间，index..货物自由选择
+    public static int process(int[] w, int[] v, int index, int rest) {
+        if (rest < 0) {
+            // 无效方案
+            return -1;
+        }
+        if (index == w.length) {
+            return 0;
+        }
+        int p1 = process(w, v, index + 1, rest);
+        int p2 = -1;
+        int p2Next = process(w, v, index + 1, rest - w[index]);
+        if (p2Next != -1) {
+            p2 = v[index] + p2Next;//有效结果
         }
         return Math.max(p1, p2);
     }
